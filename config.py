ec7a86af203e56cf1f2917d2df54edc92824adc8
Carol Li <caroldotli@gmail.com>
2020-03-15 14:41:16 -0400

    changed name from flask-base

--- config.py
+++ config.py
@@ -20,7 +20,7 @@ if os.path.exists('config.env'):
 
 
 class Config:
-    APP_NAME = os.environ.get('APP_NAME', 'Flask-Base')
+    APP_NAME = os.environ.get('APP_NAME', 'City Controller\'s Office')
 
     if os.environ.get('SECRET_KEY'):
         SECRET_KEY = os.environ.get('SECRET_KEY')
