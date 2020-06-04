c68737dc73cf44a9d4d73e7deb4b0d22f32b681f
robintan98 <46473314+robintan98@users.noreply.github.com>
2020-06-04 16:34:28 -0700

    replaced entry if duplicate record found w/ lower days remaining

--- app/contracts/views.py
+++ app/contracts/views.py
@@ -11,6 +11,8 @@ import csv
 from datetime import datetime
 
 from flask_rq import get_queue
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import exc
 
 from app import db
 from app.models import ProfServ, Profit_Status, Exempt_Status, Department, ContrType
@@ -29,6 +31,8 @@ def readCSV(filename):
         csvReader = csv.reader(csvDataFile)
         next(csvReader)
         for row in csvReader:
+            if row[0] == '':
+                continue
             # if int(row[12]) == 102:
             #    exstat = Exempt_Status.EXEMPT
             #    advext = Exempt_Status.EXEMPT
@@ -46,7 +50,9 @@ def readCSV(filename):
             else:
                 profstat = Profit_Status.Non_Profit
             contract = ProfServ(
-                id=row[0] + row[1] + row[6] + str(now),
+                # id=row[0] + row[1] + row[6] + str(now),
+                # hash logic: original contract id - current item id - start date
+                id=row[0] + '-' + row[1] + '-' + row[6],
                 original_contract_id=row[0],
                 current_item_id=row[1],
                 department_name=row[2],
@@ -64,5 +70,34 @@ def readCSV(filename):
                 profit_status=profstat,
                 timestamp=now
             )
-            db.session.add(contract)
-            db.session.commit()
+            try:
+                db.session.add(contract)
+                db.session.commit()
+            except exc.IntegrityError:
+                db.session.rollback()
+                conflict_id = row[0] + '-' + row[1] + '-' + row[6]
+                orig_contract = ProfServ.query.filter_by(id=conflict_id).first()
+                orig_days_remaining = orig_contract.days_remaining
+                dupl_days_remaining = int(row[8])
+
+                # if days_remaining of the duplicate contract has less days remaining,
+                # update ALL entries of original contract
+                if dupl_days_remaining < orig_days_remaining:
+                    orig_contract.department_name = contract.department_name
+                    orig_contract.vendor = contract.vendor
+                    orig_contract.contract_structure_type = contract.contract_structure_type
+                    orig_contract.short_desc = contract.short_desc
+                    orig_contract.end_dt = contract.end_dt
+                    orig_contract.days_remaining = contract.days_remaining
+                    orig_contract.amt = contract.amt
+                    orig_contract.tot_payments = contract.tot_payments
+                    orig_contract.orig_vendor = contract.orig_vendor
+                    orig_contract.exempt_status = contract.exempt_status
+                    orig_contract.adv_or_exempt = contract.adv_or_exempt
+                    orig_contract.profit_status = contract.profit_status
+                    orig_contract.timestamp = contract.timestamp
+
+                db.session.commit()
+                print (orig_contract.days_remaining)
+                
+                print (dupl_days_remaining)
