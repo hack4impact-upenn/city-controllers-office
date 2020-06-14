981b4e94ac03def5b2e5b9f93955ec9c2f8a7503
Daniel Tian <dtian.95@gmail.com>
2020-06-14 16:45:51 +0800

    pagenation works yay

--- app/templates/main/pagenation/src/index.js
+++ app/templates/main/pagenation/src/index.js
@@ -1,17 +1,62 @@
 import React, { useState, useEffect } from "react";
 import ReactDOM from "react-dom";
+import { Pagination } from "semantic-ui-react";
 
 const INJECT_DIV_TAG = "result-list-target";
 const container = document.getElementById(INJECT_DIV_TAG);
 
 const ResultList = () => {
+  const result_list = window.filtered_json;
+  const page_size = 20;
+  const totalPages = Math.round(result_list.length / page_size);
+  const [page, setPage] = useState({ start: 0, end: page_size });
+
+  function handlePageChange(data) {
+    const activePage = data.activePage,
+      start = (activePage - 1) * 20,
+      end = activePage * 20;
+
+    setPage({ start: start, end: end });
+  }
+
   useEffect(() => {
-    console.log(window.filtered_json);
+    console.log("variable loaded!");
+    console.log(result_list[0]);
   }, []);
   return (
-    <div>
-      <h1>Hello World!</h1>
-    </div>
+    <>
+      {result_list.slice(page.start, page.end).map((entry) => (
+        <div key={entry.original_contract_id}>
+          <div class="paper">
+            <div class="ui grid">
+              <div class="eight wide column">
+                <h3 class="theme-primary">{entry.vendor}</h3>
+                <p>
+                  <i>ID: {entry.original_contract_id}</i>
+                </p>
+              </div>
+              <div class="eight wide column">
+                <div style={{ lineHeight: "20px" }}>
+                  <div class="tag">TYPE</div>
+                  <b>{entry.contract_structure_type}</b>
+                </div>
+                <div style={{ lineHeight: "20px" }}>
+                  <div class="tag">DEPT</div>
+                  <b>{entry.department_name}</b>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      ))}
+      <div style={{ marginTop: "50px", height: "30vh" }}>
+        <Pagination
+          defaultActivePage={1}
+          totalPages={totalPages}
+          onPageChange={(event, data) => handlePageChange(data)}
+        />
+      </div>
+    </>
   );
 };
 
