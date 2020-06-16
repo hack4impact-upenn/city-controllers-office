d129eecd3bfded00e5475bed466014accdc7ab93
Carol Li <caroldotli@gmail.com>
2020-06-16 08:08:50 -0400

    Change department dropdown options to title case

--- app/main/views.py
+++ app/main/views.py
@@ -56,7 +56,7 @@ def search():
     form = ResultsForm()
     depts = Department.query.all()
     types = ContrType.query.all()
-    form.department.choices = [(dept.department_name, dept.department_name) for dept in depts]
+    form.department.choices = [(dept.department_name, dept.department_name.title()) for dept in depts]
     form.contract_type.choices = [(type.contract_structure_type, type.contract_structure_type) for type in types]
     if request.method == 'POST':
         if database_csv_form.database_csv_submit.data and database_csv_form.validate():
