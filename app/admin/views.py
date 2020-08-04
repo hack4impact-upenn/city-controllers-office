a7df99cc378e090ea3a6cde09ac100793e6a10d6
nabeelf <nfarooqui99@gmail.com>
2020-08-04 17:55:11 -0400

    took out boto

--- app/admin/views.py
+++ app/admin/views.py
@@ -8,7 +8,6 @@ from flask import (
     url_for,
     send_file,
 )
-import boto3
 from flask_login import current_user, login_required
 from flask_rq import get_queue
 from werkzeug import secure_filename
@@ -428,51 +427,3 @@ def delete_selected():
         db.session.commit()
 
     return ("Success")
-
-@admin.route('/sign-s3/')
-@admin_required
-@login_required
-def sign_s3():
-    # Load necessary information into the application
-    TARGET_FOLDER = 'json/'
-    S3_REGION = 'us-east-2'
-    S3_BUCKET = os.environ.get('S3_BUCKET')
-
-    # Load required data from the request
-    pre_file_name = request.args.get('file-name')
-    file_name = ''.join(pre_file_name.split('.')[:-1]) + \
-                str(time.time()).replace('.',  '-') + '.' + \
-                ''.join(pre_file_name.split('.')[-1:])
-    file_type = request.args.get('file-type')
-
-    # Initialise the S3 client
-    s3 = boto3.client('s3',
-                      region_name=S3_REGION,
-                      aws_access_key_id=os.environ.get('AWS_ACCESS_KEY_ID'),
-                      aws_secret_access_key=os.environ.get('AWS_SECRET_ACCESS_KEY')
-                      )
-
-    # Generate and return the presigned URL
-    presigned_post = s3.generate_presigned_post(
-        Bucket=S3_BUCKET,
-        Key=TARGET_FOLDER + file_name,
-        Fields={
-            "acl": "public-read",
-            "Content-Type": file_type
-        },
-        Conditions=[{
-            "acl": "public-read"
-        }, {
-            "Content-Type": file_type
-        }],
-        ExpiresIn=60000)
-
-    # Return the data to the client
-    return json.dumps({
-        'data':
-            presigned_post,
-        'url_upload':
-            'https://%s.%s.%s.amazonaws.com' % (S3_BUCKET, 's3', S3_REGION),
-        'url':
-            'https://%s.%s.%s.amazonaws.com/json/%s' % (S3_BUCKET, 's3', S3_REGION, file_name)
-    })
